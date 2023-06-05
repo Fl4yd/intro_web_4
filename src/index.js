b7638e02e3908b2803459601b30d267ca8754a40
Jere Arokivi <51503069+Fl4yd@users.noreply.github.com>
2023-06-05 12:05:26 +0000

    Tehtavat 1-2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const text = document.getElementById('input-show');
+const button = document.getElementById('submit-data');
+const list_body = document.getElementById('list-body');
+
+document.body.addEventListener('click', (event) => {
+  if (event.target === button) {
+    const name = text.value;
+    getMovies(name);
+  }
+});
+
+async function getMovies(movieName) {
+    const url = "https://api.tvmaze.com/search/shows?q=" + movieName;
+    const usersPromise = await fetch(url);
+    const userJSON = await usersPromise.json();
+    console.log(userJSON);
+
+    userJSON.forEach((movie) => {
+      let newMovie = document.createElement('div');
+      let image = document.createElement('img');
+      let show_info = document.createElement('div');
+      let title = document.createElement('h1');
+      let summary = movie.show.summary;
+      newMovie.setAttribute('class','show-data');
+      show_info.setAttribute('class','show-info');
+      title.innerHTML = movie.show.name;
+      show_info.appendChild(title);
+      show_info.innerHTML += summary;
+      if (movie.show.image != null) {
+        image.src = movie.show.image.medium;
+
+      }
+      newMovie.appendChild(image);
+      newMovie.appendChild(show_info);
+      list_body.appendChild(newMovie)
+    });
+}
+
+const main_div = document.createElement('div');
+const img = document.createElement('img');
+const inner_div = document.createElement('div');
+const title = document.createElement('h1')
